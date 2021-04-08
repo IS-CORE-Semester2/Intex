c971e2a54aacfd8d057725c4c40580a92a9f8c45
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-04-08 13:47:07 -0600

    CRUD finished for Users/Roles; Authorization working but need to start specifying on specific pages

--- Data/Migrations/20210408184207_Test.Designer.cs
+++ Data/Migrations/20210408184207_Test.Designer.cs
@@ -4,14 +4,16 @@ using Intex.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Intex.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210408184207_Test")]
+    partial class Test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
