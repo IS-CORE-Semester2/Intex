2043d371f1eb739d7e9579a3a712be8845672260
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-04-09 13:21:44 -0600

    Added Keys

--- Models/Cranial2002.cs
+++ Models/Cranial2002.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,6 +10,7 @@ namespace Intex
 {
     public partial class Cranial2002
     {
+        [Key]
         public int? SampleNumber { get; set; }
         public double? MaximumCranialLength { get; set; }
         public double? MaximumCranialBreadth { get; set; }
--- Models/OracleSpread.cs
+++ Models/OracleSpread.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,6 +10,7 @@ namespace Intex
 {
     public partial class OracleSpread
     {
+        [Key]
         public int? Gamous { get; set; }
         public int? BurialSquare { get; set; }
         public string Nors { get; set; }
