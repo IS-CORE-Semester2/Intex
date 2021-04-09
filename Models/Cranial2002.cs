3ea79b268548c5d2b2f6b4dc699e8bd8ecaf738b
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-04-09 13:21:02 -0600

    Last 2 models

---
+++ Models/Cranial2002.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Intex
+{
+    public partial class Cranial2002
+    {
+        public int? SampleNumber { get; set; }
+        public double? MaximumCranialLength { get; set; }
+        public double? MaximumCranialBreadth { get; set; }
+        public double? BasionBreg { get; set; }
+        public double? BasionNasion { get; set; }
+        public double? BasionProsthionLength { get; set; }
+        public double? BizygomaticDiameter { get; set; }
+        public double? NasionProsthion { get; set; }
+        public double? MaximumNasalBreadth { get; set; }
+        public double? InterorbitalBreadth { get; set; }
+        public string BurialPositioningNorthSouthNumber { get; set; }
+        public string BurialPositioningNorthSouthDirection { get; set; }
+        public string BurialPositioningEastWestNumber { get; set; }
+        public string BurialPositioningEastWestDirection { get; set; }
+        public int? BurialNumber { get; set; }
+        public double? BurialDepth { get; set; }
+        public string BurialSubPlotDirection { get; set; }
+        public string BurialArtifactDescription { get; set; }
+        public bool? BuriedWithArtifacts { get; set; }
+        public string GilesGender { get; set; }
+        public string BodyGender { get; set; }
+    }
+}
---
+++ Models/OracleSpread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Intex
+{
+    public partial class OracleSpread
+    {
+        public int? Gamous { get; set; }
+        public int? BurialSquare { get; set; }
+        public string Nors { get; set; }
+        public int? Sq2 { get; set; }
+        public string Eorw { get; set; }
+        public string Area { get; set; }
+        public int? Burialnum { get; set; }
+        public double? Westtohead { get; set; }
+        public double? Westtofeet { get; set; }
+        public double? Southtohead { get; set; }
+        public double? Southtofeet { get; set; }
+        public double? Depth { get; set; }
+        public string Preservation { get; set; }
+        public string Burialicon { get; set; }
+        public string Sex { get; set; }
+        public string Sexmethod { get; set; }
+        public string Ageatdeath { get; set; }
+        public string Agemethod { get; set; }
+        public string Haircolor { get; set; }
+        public bool? Sample { get; set; }
+    }
+}
