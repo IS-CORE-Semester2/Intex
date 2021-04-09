98100ee92b53aed1eec162f635aab35b2fe0586d
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-04-08 18:19:09 -0600

    Styling Changes

---
+++ Models/Burial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex.Models
+{
+    public class Burial
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+    }
+}
