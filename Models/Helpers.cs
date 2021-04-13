445eda9de816f382a6587df18628f1eab4a73fcc
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-04-12 21:45:25 -0600

    RDS setup

--- Models/Helpers.cs
+++ Models/Helpers.cs
@@ -23,7 +23,7 @@ namespace Intex.Models
             string port = appConfig["1433"];
 
             //return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
-            return "Data Source=aa4olymoet4lpi.cww7zl3zl4yd.us-east-1.rds.amazonaws.com;Initial Catalog=IntexII;User ID=dbuser;Password=intexgroup29;MultipleActiveResultSets=true;";
+            return "Data Source=aaw4kdw5iaqlen.cww7zl3zl4yd.us-east-1.rds.amazonaws.com; Initial Catalog=IntexII;User ID=dbuser;Password=intexgroup29;MultipleActiveResultSets=true;";
             //"DefaultConnection": "Host=aa334sqauw5ixt.cww7zl3zl4yd.us-east-1.rds.amazonaws.com;Port=1433;Username=EpicWerf;Password=JacksonRocks;Database=aa334sqauw5ixt"
 
             //return "Data Source=aa15qhx9s42mgmt.chgpbo5ixplt.us-east-1.rds.amazonaws.com;Initial Catalog=ebdb;User ID=dbuser;Password=group211;";
